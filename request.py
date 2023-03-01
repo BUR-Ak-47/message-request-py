a1f6721d624ea32586b5c3f229d6b408e997289f
Burak <124401463+BUR-Ak-47@users.noreply.github.com>
2023-03-01 19:57:26 +0100

    Update request.py

--- request.py
+++ request.py
@@ -1,1 +1,13 @@
-wait
+import requests
+
+# https://discord.com/api/vw9/channels/CHANEL_ID/messages
+
+payload = {
+  'content': "hello world"
+}
+
+header = {
+  'authorization': 'TOKEN'
+}
+
+r = requests.post ("https://discord.com/api/v9/channels/CHANEL_ID/messages", data=payload, headers=header)
